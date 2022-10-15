f42e686302c63a5538878cdc075c166d26ff3d2c
Gustavo <gustavohernandezmartinez6@gmail.com>
2022-10-15 11:06:54 -0500

    Funcion multiplicacion creada como parte de los requerimientos del cliente

--- main.c
+++ main.c
@@ -8,10 +8,15 @@ int resta(int x, int y){
     return x - y;
 }
 
+int multiplicacion(int x, int y){
+    return x * y;
+}
+
 int main(){
     printf("Hola Mundo en C\n");
 
     printf("Suma = %d\n",suma(5,6));
     printf("Resta = %d\n",resta(10,4));
+    printf("Multiplicacion = %d\n",multiplicacion(7,5));
     return 0;
 }
