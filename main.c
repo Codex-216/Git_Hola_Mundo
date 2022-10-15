c18229d88b243e5cd6f81800ef458d647db55e8e
Gustavo <gustavohernandezmartinez6@gmail.com>
2022-10-15 10:59:13 -0500

    Funciones suma y resta solicitadas por el cliente

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int suma(int x, int y){
+    return x + y;
+}
+
+int resta(int x, int y){
+    return x - y;
+}
+
 int main(){
-    printf("Hola Mundo en C");
+    printf("Hola Mundo en C\n");
+
+    printf("Suma = %d\n",suma(5,6));
+    printf("Resta = %d\n",resta(10,4));
     return 0;
 }
